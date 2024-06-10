a6d41f12c654a6527ff62c380146798e48d5198e
JaredVS777 <jared.valenzuelajvs02@gmail.com>
2024-06-10 03:36:23 -0500

    App para subir archivos con Storage

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBoO8gEJo-VQFfPS2aY7xdu8mVVbdU5dAI",
+    authDomain: "chat-7135c.firebaseapp.com",
+    projectId: "chat-7135c",
+    storageBucket: "chat-7135c.appspot.com",
+    messagingSenderId: "386282013818",
+    appId: "1:386282013818:web:2570886e609822151ca7c3"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
