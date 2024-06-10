db45d762e2601cd513596e4d5ad6ba682b33e137
JaredVS777 <jared.valenzuelajvs02@gmail.com>
2024-06-10 14:57:36 -0500

    Variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyD7xx3n2B96SPBO0mf966a31nSrA3_OC1g",
-    authDomain: "storage-f5a23.firebaseapp.com",
-    projectId: "storage-f5a23",
-    storageBucket: "storage-f5a23.appspot.com",
-    messagingSenderId: "327049308520",
-    appId: "1:327049308520:web:dbdab5a3912ddc6f094edd"
+
   }
 };
